e22e2b287efba49a8a3e2267984ff2c27201f954
TonWonton <154277764+TonWonton@users.noreply.github.com>
2023-12-18 23:37:12 +0100

    Delete HC_FXsettings directory

--- HC_FXsettings/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-using static BepInEx.HC_FXsettings;
-
-[assembly: AssemblyTitle(GUID)]
-[assembly: AssemblyDescription(PluginName)]
-[assembly: AssemblyProduct(GUID)]
-[assembly: AssemblyVersion(PluginVersion)]
-[assembly: AssemblyFileVersion(PluginVersion)]
