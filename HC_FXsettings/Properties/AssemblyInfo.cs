e8e9d5a1af254e9effe1d220f1f90a1349495cbf
TonWonton <154277764+TonWonton@users.noreply.github.com>
2023-12-18 23:03:52 +0100

    Add files via upload

---
+++ HC_FXsettings/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using static BepInEx.HC_FXsettings;
+
+[assembly: AssemblyTitle(GUID)]
+[assembly: AssemblyDescription(PluginName)]
+[assembly: AssemblyProduct(GUID)]
+[assembly: AssemblyVersion(PluginVersion)]
+[assembly: AssemblyFileVersion(PluginVersion)]
