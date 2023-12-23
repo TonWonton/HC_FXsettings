7d6aaa817575232ef8f9a0b693bd48b993473ffd
TonWonton <154277764+TonWonton@users.noreply.github.com>
2023-12-23 19:36:02 +0100

    Add files via upload
    Only DigitalCraft

---
+++ HC_FXsettingsDolce/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+namespace FXsettings
+{
+    internal class FXsettingsInfo
+    {
+        public static class PluginInfo
+        {
+            public const string PLUGIN_GUID = "HC_FXsettings";
+            public const string PLUGIN_NAME = "HC_FXsettings";
+            public const string PLUGIN_VERSION = "1.2.0";
+        }
+    }
+}
